d92937e67729f610b11810cb6b3546c6a66d28d4
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-04-26 11:03:52 +0200

    feat(Contact): add optional data

--- src/fr/dieunelson/esgi/phone/Contact.java
+++ src/fr/dieunelson/esgi/phone/Contact.java
@@ -1,13 +1,18 @@
 package fr.dieunelson.esgi.phone;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Contact {
 
     private String name;
     private String phoneNumber;
+    private HashMap<String, String> data;
 
     public Contact(String name, String phoneNumber) {
         this.name = name.trim();
         this.phoneNumber = phoneNumber.trim();
+        this.data = new HashMap<>();
     }
 
     public String getName() {
@@ -26,4 +31,19 @@ public class Contact {
         this.phoneNumber = phoneNumber.trim();
     }
 
+    public void add(String name, String value) {
+        this.data.put(name.trim(), value.trim());
+    }
+
+    public String get(String key) {
+        return this.data.get(key);
+    }
+
+    public String delete(String key) {
+        return this.data.remove(key);
+    }
+
+    public Set<String> getDataKey() {
+        return this.data.keySet();
+    }
 }
